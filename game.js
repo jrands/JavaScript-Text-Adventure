5f430079fc3a68300fe5168383e1a34693c97d85
John Randis <John.Randis1@Marist.edu>
2014-11-04 15:12:27 -0500

    Experimenting with the DOM

--- game.js
+++ game.js
@@ -1,8 +1,18 @@
-var pickUp = function(item){
-  var items = element.querySelector("#inventory");
-  this.items.push(item);
-}
+var inputBox = document.querySelector('#action');
 
+console.log(inputBox.tagName);
+console.log(inputBox.parentNode.tagName);
+
+var title = document.querySelector('#header')
+title.innerHTML = "Hello!"
+
+var newParagraph = function(txt){
+  var newPara = document.createElement("p");
+  var newText = document.createTextNode(txt);
+  newPara.appendChild(newText);
+  document.body.appendChild(newPara);
+}
 var checkItem = function(){
-  
+  var inv = document.querySelector("#inventory");
+  output.innerHTML = "You currently have in your posession: " + inv;
 }
