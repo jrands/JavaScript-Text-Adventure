31c7d63630d886a6cd0d226a0a1341ac01670dcb
John <John.Randis1@Marist.edu>
2014-12-04 23:42:06 -0500

    Fixed interpret and report function, location description now displays

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 // Global player object (later this will no longer be global)
 var player = {
-	items: ["tool kit", "light"],
+	items: [],
+	location: "",
 	pickup: function(item){
 		this.items.push(item);
 	},
@@ -14,20 +15,17 @@ var player = {
 
 // parse and normalize the user input string
 function interpret (input) {
-  var i1 = input.trim().toLowerCase();
-  var i2 = i1.split(" ");
-  var result = [];
-  result.push(i2.shift());
-  result.push(i2.join(" "));
-  return result;
+  console.log("calling interpret");
+  var cmd = {}, tokens = input.trim().toLowerCase().split(" ");
+  cmd.action = tokens.shift();
+  cmd.target = tokens.join(" ");
+  return cmd;
 }
 
 // perform the desired player action
 function execute (command) {
-	var action = command.shift();
-  var object = command.shift();
-  var method = player[action];
-  method(object);
+	console.log("calling execute"); // remove when finished debugging
+  player[command.action](command.target);
 }
 
 // display any results/changes on the page
@@ -36,6 +34,8 @@ function report () {
   for (var i = 0; i < player.items.length; i++){
     note.innerHTML = player.items[i];
   }
+  console.log(map.locations.area0);
+  console.log(player.items);
 }
 
 // run one pass of the game loop
@@ -43,6 +43,7 @@ function gameStep (input) {
 	var cmd = interpret(input); // parse the user input
 	var result = execute(cmd); // run the desired command
 	report(result); // display the results on the screen
+	
 }
 
 var gameStart = function() {
--- location.js
+++ location.js
@@ -21,29 +21,29 @@ map.locations = {
 
   area3: new Location ("ER Room 105", "A zombie is in this room. The room contains a dresser, TV, and nightstand just like 107."),
 
-  area4: new Location ("Janitorial Closet", "The closet contains lots of cleaning supplies. You see various tools such as a bucket, a mop, and rubber gloves.")
+  area4: new Location ("Janitorial Closet", "The closet contains lots of cleaning supplies. You see various tools such as a bucket, a mop, and rubber gloves."),
+  
+  area5: new Location ("Office", "An office belonging to head surgeon. The charts and graphs that line the wall make little sense to you. The desk with all it's knick-knacks in the center of the room catches your eye."),
+  
+  area6: new Location ("Lobby",""),
+  
+  area7: new Location ("Parking Lot", "The parking lot is virtually empty. The few remaining cars have been siphoned of gas.")
 };
+
+//document.querySelector('#descrip').innerHTML = area0.description;
 
 map.connections = [
 
-  [0, 1, 1, 0, 0]
-  [1, 0, 0, 0, 0]
-  [1, 0, 0, 1, 1]
-  [0, 0, 1, 0, 0]
-  [0, 0, 1, 0, 0]
+  [0, 1, 1, 0, 0, 0, 0, 0], //room 107
+  [1, 0, 0, 0, 0, 0, 0, 0], //bathroom
+  [1, 0, 0, 1, 1, 1, 1, 0], //hallway
+  [0, 0, 1, 0, 0, 0, 0, 0], //room 105
+  [0, 0, 1, 0, 0, 0, 0, 0], //janitorial closet
+  [0, 0, 1, 0, 0, 0, 0, 0] //office
+  [0, 0, 1, 0, 0, 0, 0, 1] //lobby
+  [0, 0, 0, 0, 0, 1, 0, 0] //parking lot
   
 ];
 
-map.test = function(){
-
-  for (var i = 0; i < 5; i++){
-    if ([0][i] === 1)
-      console.log([0][i].name)
-  }
-
-};
-
-map.test();
-
-
-
+var note = document.querySelector('#descrip');
+note.innerHTML = map.locations.area0.description;
